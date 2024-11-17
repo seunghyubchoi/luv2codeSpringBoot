ffc688964f2a1d9c0727780a3d6185bac6e6fcf4
Seunghyub Choi <seunghyub.choi@gmail.com>
2024-11-17 16:38:42 +0900

    data biding by using modelattribute

--- 09-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 09-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.luv2code.springboot.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -20,4 +22,12 @@ public class StudentController {
         return "student-form";
     }
 
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+
+        // log the input data
+        System.out.println("theStudent : " + theStudent.getFirstName() + " " + theStudent.getLastName());
+
+        return "student-confirmation";
+    }
 }
