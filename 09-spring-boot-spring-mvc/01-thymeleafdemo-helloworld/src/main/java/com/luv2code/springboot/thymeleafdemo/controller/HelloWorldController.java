ef1c3d8d35b91be09405f013d085e6032513d5f8
Seunghyub Choi <seunghyub.choi@gmail.com>
2024-11-17 16:22:51 +0900

    RequestParam addeD

--- 09-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 09-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -3,13 +3,16 @@ package com.luv2code.springboot.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
     // new a controller method to show initial HTML form
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm() {
         return "helloworld-form";
     }
@@ -35,6 +38,18 @@ public class HelloWorldController {
         String result = "Yo! " + theName;
 
         // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @PostMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        theName = theName.toUpperCase();
+
+        String result = "Hey My Friend from v3! " + theName;
+
         model.addAttribute("message", result);
 
         return "helloworld";
