6319ba4e72ffc722b10ef218a01886aca4c83e84
Seunghyub Choi <seunghyub.choi@gmail.com>
2024-12-02 21:14:52 +0900

    requestMathers.hasRole

--- 11-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
+++ 11-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
@@ -16,4 +16,10 @@ public class DemoController {
     public String showLeaders() {
         return "leaders";
     }
+
+    // add request mapping for /systems
+    @GetMapping("/systems")
+    public String showSystems() {
+        return "systems";
+    }
 }
